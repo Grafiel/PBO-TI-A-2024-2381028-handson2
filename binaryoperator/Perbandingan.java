a65c00fea8d19fe24cac2e03fac9c011e4bbcdbf
Grafiel Sitanggang <2381028@unai.edu>
2024-09-05 10:27:27 +0900

    Handson 2

---
+++ binaryoperator/Perbandingan.java
@@ -0,0 +1,23 @@
+package binaryoperator;
+
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        boolean hasilSamaDengan = (a = b);
+        System.out.println("a = b: " + hasilSamaDengan);
+
+        boolean hasilLebihBesarDari = (a > b);
+        System.out.println("a > b: " + hasilLebihBesarDari);
+
+        boolean hasilLebihKecilDari = (a < b);
+        System.out.println("a < b: " + hasilLebihKecilDari);
+
+        boolean hasilLebihBesarSamaDengan = (a >= b);
+        System.out.println("a >= b: " + hasilLebihBesarSamaDengan);
+
+        boolean hasilLebihKecilSamaDengan = (a <= b);
+        System.out.println("a < b: " + hasilLebihKecilSamaDengan);
+    }
+}
---
+++ branching/IfStatement.java
@@ -0,0 +1,38 @@
+package branching;
+
+import jdk.jshell.Snippet;
+
+import javax.xml.transform.Source;
+import  java.util.Scanner;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(Source:System.in);
+
+        System.out.println("Masukan nomor hari (1 - 7): ");
+        int nomorHari = input.nextInt();
+
+        String namaHari = "";
+
+        if (nomorHari == 1) {
+            namaHari = "Minggu";
+        } else if (nomorHari == 2) {
+            namaHari = "Senin";
+        } else if (nomorHari == 3) {
+            namaHari = "Selasa";
+        } else if (nomorHari == 4) {
+            namaHari = "Rabu";
+        } else if (nomorHari == 5) {
+            namaHari = "Kamis";
+        } else if (nomorHari == 6) {
+            namaHari = "Jumat";
+        } else if (nomorHari == 7) {
+            namaHari = "Sabtu";
+        } else {
+            System.out.println("Masukkan nomor antara 1 - 7");
+            System.exit();
+        }
+
+        System.out.println("Hari : " + namaHari);
+    }
+}
